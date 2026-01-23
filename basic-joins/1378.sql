c94f59869a67bcd9de539d9502a21007cdfe3423
Jomon Joy <252370917+itsjomon-temp@users.noreply.github.com>
2026-01-23 09:52:29 +0530

    Create 1378.sql

---
+++ basic-joins/1378.sql
@@ -0,0 +1,12 @@
+# 1378. Replace Employee ID With The Unique Identifier
+
+# Write your MySQL query statement below
+SELECT 
+    eu.unique_id, 
+    e.name
+FROM 
+    employees e
+LEFT JOIN 
+    employeeuni eu 
+ON 
+    e.id = eu.id;
