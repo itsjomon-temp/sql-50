1345ed7ef7879a31a42712670864724f56657b35
Jomon Joy <252370917+itsjomon-temp@users.noreply.github.com>
2026-01-23 09:54:15 +0530

    Create 197.sql

---
+++ basic-joins/197.sql
@@ -0,0 +1,13 @@
+# 197. Rising Temperature
+
+# Write your MySQL query statement below
+SELECT 
+    w1.id
+FROM 
+    Weather w1
+JOIN 
+    Weather w2
+ON 
+    SUBDATE(w1.recordDate, 1) = w2.recordDate
+AND 
+    w1.temperature > w2.temperature;
