62d13a71c0700d0f6b08095958837b2e10d6d60f
Jomon Joy <252370917+itsjomon-temp@users.noreply.github.com>
2026-01-24 08:59:17 +0530

    Create 1211.sql

---
+++ basic-aggregate-functions/1211.sql
@@ -0,0 +1,13 @@
+# 1211. Queries Quality and Percentage
+
+# Write your MySQL query statement below
+SELECT 
+    query_name,
+    ROUND(AVG(rating / position), 2) AS quality,
+    ROUND(
+        AVG(CASE WHEN rating < 3 THEN 1 ELSE 0 END) * 100,
+        2
+    ) AS poor_query_percentage
+FROM queries
+WHERE query_name IS NOT NULL
+GROUP BY query_name;
