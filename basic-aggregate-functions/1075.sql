fcfb20ef6880501ff91b54f3482c130e8725835e
Jomon Joy <252370917+itsjomon-temp@users.noreply.github.com>
2026-01-24 08:58:26 +0530

    Create 1075.sql

---
+++ basic-aggregate-functions/1075.sql
@@ -0,0 +1,10 @@
+# 1075. Project Employees I
+
+# Write your MySQL query statement below
+SELECT 
+    p.project_id, 
+    ROUND(AVG(e.experience_years), 2) AS average_years
+FROM project p
+JOIN employee e 
+    ON p.employee_id = e.employee_id
+GROUP BY p.project_id;
