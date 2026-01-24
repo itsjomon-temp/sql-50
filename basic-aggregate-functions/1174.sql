8736729c57a29726a607b4f61dc937355bf1a229
Jomon Joy <252370917+itsjomon-temp@users.noreply.github.com>
2026-01-24 09:00:11 +0530

    Create 1174.sql

---
+++ basic-aggregate-functions/1174.sql
@@ -0,0 +1,15 @@
+# 1174. Immediate Food Delivery II
+
+# Write your MySQL query statement below
+SELECT 
+    ROUND(
+        AVG(IF(min_order_date = min_customer_pref_delivery_date, 1, 0)) * 100,
+        2
+    ) AS immediate_percentage
+FROM (
+    SELECT 
+        MIN(order_date) AS min_order_date, 
+        MIN(customer_pref_delivery_date) AS min_customer_pref_delivery_date
+    FROM Delivery
+    GROUP BY customer_id
+) AS min_delivery_table;
