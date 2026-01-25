c47c2fc6eaaaaae1cab7708024f5861867e55045
Jomon Joy <252370917+itsjomon-temp@users.noreply.github.com>
2026-01-25 11:04:54 +0530

    Create 2356.sql

---
+++ sorting-and-grouping/2356.sql
@@ -0,0 +1,10 @@
+# 2356. Number of Unique Subjects Taught by Each Teacher
+
+# Write your MySQL query statement below
+SELECT 
+    teacher_id,
+    COUNT(DISTINCT subject_id) AS cnt
+FROM 
+    teacher
+GROUP BY 
+    teacher_id;
