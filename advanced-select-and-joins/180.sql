00fdf5f8a1d07996d3e3535de084988311141d4f
Jomon Joy <252370917+itsjomon-temp@users.noreply.github.com>
2026-01-26 09:31:24 +0530

    Create 180.sql

---
+++ advanced-select-and-joins/180.sql
@@ -0,0 +1,14 @@
+# 180. Consecutive Numbers
+
+# Write your MySQL query statement below
+SELECT 
+    l1.num AS ConsecutiveNums
+FROM logs l1, logs l2, logs l3
+WHERE 
+    l1.id - l2.id = 1
+    AND l2.id - l3.id = 1
+    AND l1.num = l2.num
+    AND l2.num = l3.num
+    AND l1.num = l3.num
+GROUP BY 
+    l1.num;
