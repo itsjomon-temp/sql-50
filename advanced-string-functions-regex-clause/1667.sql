99993e1b5bad535a435eab7b1b5c36cdc30d2146
Jomon Joy <252370917+itsjomon-temp@users.noreply.github.com>
2026-01-28 11:37:19 +0530

    Create 1667.sql

---
+++ advanced-string-functions-regex-clause/1667.sql
@@ -0,0 +1,11 @@
+# 1667. Fix Names in a Table
+
+# Write your MySQL query statement below
+SELECT 
+    user_id,
+    CONCAT(
+        UPPER(LEFT(name, 1)),
+        LOWER(RIGHT(name, LENGTH(name) - 1))
+    ) AS name
+FROM users
+ORDER BY user_id;
