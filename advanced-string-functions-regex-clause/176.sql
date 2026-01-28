335e6c5a87fc39917c0eed608f6243b885bd7c3d
Jomon Joy <252370917+itsjomon-temp@users.noreply.github.com>
2026-01-28 11:38:40 +0530

    Create 176.sql

---
+++ advanced-string-functions-regex-clause/176.sql
@@ -0,0 +1,10 @@
+# 176. Second Highest Salary
+
+# Write your MySQL query statement below
+SELECT 
+    MAX(salary) AS SecondHighestSalary
+FROM Employee
+WHERE salary NOT IN (
+    SELECT MAX(salary)
+    FROM Employee
+);
