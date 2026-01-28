e5c2b4b88ab3ad6cc356b9b3ccdae37ccf581c67
Jomon Joy <252370917+itsjomon-temp@users.noreply.github.com>
2026-01-28 11:40:00 +0530

    Create 1517.sql

---
+++ advanced-string-functions-regex-clause/1517.sql
@@ -0,0 +1,7 @@
+# 1517. Find Users With Valid E-Mails
+
+# Write your MySQL query statement below
+SELECT *
+FROM Users
+WHERE mail REGEXP '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode\\.com$'
+  AND BINARY mail LIKE '%@leetcode.com';
